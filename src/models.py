66712e5cb0cd41345e42e1ffa92458d954359bfa
obadrawi <omar.elbadrawi@sap.com>
2019-12-08 16:09:41 +0100

    update to 2D

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential,Model
-from keras.layers import Dense, LSTM, Embedding,Dropout,SpatialDropout1D,Conv1D,MaxPooling1D,GRU
-from keras.layers import Input,Bidirectional,GlobalAveragePooling1D,GlobalMaxPooling1D,concatenate
+from keras.layers import Dense, LSTM, Embedding,Dropout,SpatialDropout2D,Conv2D,MaxPooling2D,GRU
+from keras.layers import Input,Bidirectional,GlobalAveragePooling2D,GlobalMaxPooling2D,concatenate
 from keras import backend as K
 from attention_layer import AttentionDecoder
 from bert_keras import BertLayer
@@ -29,8 +29,8 @@ class Models:
                       input_length=150,
                       trainable=False))
         #self.model.add(SpatialDropout1D(0.2))
-        self.model.add(MaxPooling1D(pool_size=2))
-        self.model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=2))
+        self.model.add(Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'))
         self.model.add(LSTM(128,return_sequences=True))
         self.model.add(LSTM(128, return_sequences=True))
         # self.model.add(SpatialDropout1D(0.2))
@@ -56,7 +56,7 @@ class Models:
         )(self.sequence_input)
         print("****shape*****")
         print(embedding.shape)
-        base = SpatialDropout1D(self.spatial_dropout)(embedding)
+        base = SpatialDropout2D(self.spatial_dropout)(embedding)
         return base
     def build_Base_Bert_model(self):
         in_id = Input(shape=(self.max_sequence_length,), name="input_ids")
@@ -67,7 +67,7 @@ class Models:
         # Instantiate the custom Bert Layer defined above
         # bertlayer = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1",
         #                     trainable=True)
-        bert_output = BertLayer(n_fine_tune_layers=10)(self.bert_inputs)
+        bert_output = BertLayer(n_fine_tune_layers=10,)(self.bert_inputs)
         tf.reshape(bert_output,[None,bert_output.shape[0],bert_output.shape[1]])
         print("********print bert******")
         print(bert_output)
@@ -75,8 +75,8 @@ class Models:
     def build_GRU_model(self,base):
         base = GRU(128, return_sequences=True)(base)
         base = GRU(128, return_sequences=True)(base)
-        avg = GlobalAveragePooling1D()(base)
-        max = GlobalMaxPooling1D()(base)
+        avg = GlobalAveragePooling2D()(base)
+        max = GlobalMaxPooling2D()(base)
         return concatenate([avg, max])
     def build_BLTSM_model(self,base):
         base = Bidirectional(
@@ -90,20 +90,20 @@ class Models:
                  dropout=self.lstm_dropout, recurrent_dropout=self.recurrent_dropout)
 
         )(base)
-        avg = GlobalAveragePooling1D()(base)
-        max = GlobalMaxPooling1D()(base)
+        avg = GlobalAveragePooling2D()(base)
+        max = GlobalMaxPooling2D()(base)
         return concatenate([avg, max])
 
 
     def build_CNN_model(self,base):
-        base = MaxPooling1D(pool_size=2)(base)
-        base = Conv1D(self.filters, kernel_size=self.kernel_size, padding='valid',
+        base = MaxPooling2D(pool_size=2)(base)
+        base = Conv2D(self.filters, kernel_size=self.kernel_size, padding='valid',
                       kernel_initializer='glorot_uniform')(base)
-        base = MaxPooling1D(pool_size=2)(base)
-        base = Conv1D(self.filters, kernel_size=self.kernel_size, padding='valid',
+        base = MaxPooling2D(pool_size=2)(base)
+        base = Conv2D(self.filters, kernel_size=self.kernel_size, padding='valid',
                       kernel_initializer='glorot_uniform')(base)
-        avg = GlobalAveragePooling1D()(base)
-        max = GlobalMaxPooling1D()(base)
+        avg = GlobalAveragePooling2D()(base)
+        max = GlobalMaxPooling2D()(base)
         return concatenate([avg,max])
 
     def build_myModel(self,embedding_matrix,bert=True):
