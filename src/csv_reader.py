6a7e0c44b91849594011ea12353b2fe8d97aabbc
Badrawi <omar_badrawi@hotmail.com>
2020-01-01 17:37:32 +0100

    fix

--- src/csv_reader.py
+++ src/csv_reader.py
@@ -6,7 +6,7 @@ class CSVReader:
     @staticmethod
     def dataframe_from_file():
         fpath = "glue_data/MRPC/train.tsv"
-        data = pd.read_csv(fpath,sep='/t')
+        data = pd.read_csv(fpath,sep='\t')
         data =data.dropna()
         return data
 
