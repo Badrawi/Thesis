87fe25994d66057abe5e42fee80b8bdc2a62579b
Badrawi <omar_badrawi@hotmail.com>
2020-01-02 14:46:58 +0100

    return back csv reader

--- src/csv_reader.py
+++ src/csv_reader.py
@@ -4,9 +4,10 @@ from text_preprocessing import TextPreprocessing
 import xlrd
 class CSVReader:
     @staticmethod
-    def dataframe_from_file():
-        fpath = "glue_data/MRPC/train.tsv"
-        data = pd.read_csv(fpath,sep='\t', error_bad_lines=False)
+    def dataframe_from_file(file,cols):
+        fpath = "../data/"+file
+        data = pd.read_csv(fpath,sep=',',
+             quotechar="'",usecols=cols)
         data =data.dropna()
         return data
 
